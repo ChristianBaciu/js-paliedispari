78d8b454bee198c042f41d05296397127b6a082c
ChristianBaciu <christian3110@outlook.it>
2024-01-15 17:09:33 +0100

    prove

---
+++ Palidroma/main.js
@@ -0,0 +1,18 @@
+// Definizione di una funzione chiamata somma
+function somma(a, b){
+    alert (a + b);
+}
+
+function somma(a, b){
+    alert (a + b);
+}
+
+  
+  
+// somma 3 e 4, 5 e 80
+var risultato = somma(5, 5);
+var risultato2 = somma(5, 80);
+// 10
+console.log(risultato);
+// 85
+console.log(risultato2);
